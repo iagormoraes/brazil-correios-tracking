5a1fd614c4df0b2122c12a7cd36ba22aec875f5d
Ryan Marken <marken.ryan@gmail.com>
2019-09-25 22:50:33 -0400

    array params, unique result

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ const app = express();
 const getPackageInfo = require("./getPackageInfo");
 
 //example: http://localhost:8081/package/OH756347841BR
-app.get("/package/:package", async (req, res) => {
+app.get("/package", async (req, res) => {
   try {
-    const packInfo = await getPackageInfo(req.params.package);
+    console.log('req.query.package', req.query);
+
+    let promises = req.query.package.map(package => {
+      return getPackageInfo(package);
+    });
+    let resolvedPromises = await Promise.all(promises);
+    let flags = [];
+    let results = [];
+    let index = 0;
+    console.log(resolvedPromises);
+
+    resolvedPromises.forEach(el =>{
+      console.log(el);
+      el.packHistory.forEach(pack =>{
+        console.log('pack: ', pack);
+        console.log('flags:', flags);
+        if (!flags[JSON.stringify(pack)]){
+          flags[JSON.stringify(pack)] = true;
+          results.push(pack);
+        }
+      })
+    });
+
+    console.log('---------------');
+    console.log(results);
+
+    const packInfo = await getPackageInfo(req.query.package);
 
     res.status(200).json(packInfo);
   } catch (error) {
@@ -13,4 +39,4 @@ app.get("/package/:package", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 8081, () => console.log("server running"));
+app.listen(process.env.PORT || 8082, () => console.log("server running"));
