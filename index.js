e36ccd1711ccf16c6e45bbd1518885ec1be2d60f
Mathews Lima <mathews.limaa@gmail.com>
2019-10-01 23:16:54 -0300

    [fix] format the code based in the eslint/prettier

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const getPackageInfo = require("./getPackageInfo");
+const getPackageInfo = require('./getPackageInfo');
 
 //example: http://localhost:8081/package/OH756347841BR
-app.get("/package/:package", async (req, res) => {
-  try {
-    const packInfo = await getPackageInfo(req.params.package);
+app.get('/package/:package', async (req, res) => {
+    try {
+        const packInfo = await getPackageInfo(req.params.package);
 
-    res.status(200).json(packInfo);
-  } catch (error) {
-    res.status(404).json(error);
-  }
+        res.status(200).json(packInfo);
+    } catch (error) {
+        res.status(404).json(error);
+    }
 });
 
-app.listen(process.env.PORT || 8081, () => console.log("server running"));
+app.listen(process.env.PORT || 8081, () => console.log('server running'));
